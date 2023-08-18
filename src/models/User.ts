7dc6430ec5154d898142cbcd0fb802421e620c1e
Mostafa Kreidly <mkreidly91@gmail.com>
2023-08-19 01:32:57 +0300

    Backend: Created basic user model

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,1 +1,16 @@
-//hello
+import mongoose, { mongo } from 'mongoose';
+
+const UserSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: {
+    type: String,
+    required: [true, 'Email field is required'],
+    unique: [true, 'Email is already taken'],
+    match: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}/,
+  },
+  password: { type: String, required: true, select: false },
+});
+
+const UserModel = mongoose.model('User', UserSchema);
+
+export default UserModel;
