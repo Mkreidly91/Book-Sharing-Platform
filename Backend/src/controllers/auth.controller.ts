fb35716f91e2b6540b01180d07e22dd87d2d7abe
Mostafa Kreidly <mkreidly91@gmail.com>
2023-08-19 20:51:48 +0300

    Backend: Fixed auth controller giving wrong responses

--- Backend/src/controllers/auth.controller.ts
+++ Backend/src/controllers/auth.controller.ts
@@ -4,22 +4,24 @@ import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 
 const register = async (req: express.Request, res: express.Response) => {
-  const { body } = req;
-  const { name, email, password } = body;
-
-  if (!name) {
-    res.status(400).send({ message: 'Name is required' });
-  }
-  if (!email) {
-    res.status(400).send({ message: 'Email is required' });
-  }
-  if (!password) {
-    res.status(400).send({ message: 'Password is required' });
-  }
-
-  const hashedPassword = await bcrypt.hash(password, 10);
-
   try {
+    const { body } = req;
+    const { name, email, password } = body;
+
+    if (!name) {
+      return res.status(400).send({ error: 'Name is required' });
+    }
+    if (!email) {
+      return res.status(400).send({ error: 'Email is required' });
+    }
+    if (!password) {
+      return res.status(400).send({ error: 'Password is required' });
+    } else if (password.length < 6) {
+      return res.status(400).send({ error: 'Password too short' });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
     const user = new UserModel({
       name,
       email,
@@ -27,22 +29,33 @@ const register = async (req: express.Request, res: express.Response) => {
     });
 
     await user.save();
-    res.status(200).send(user);
+    return res.status(200).send({ message: 'User successfully registered.' });
   } catch (error) {
-    res.status(500).send(error);
+    if (error.code === 11000) {
+      return res.status(400).send({ error: 'Email already exists' });
+    }
+    return res.status(500).send(error);
   }
 };
 
 const login = async (req: express.Request, res: express.Response) => {
   try {
     const { email: login, password } = req.body;
+
+    if (!login) {
+      return res.status(400).send({ error: 'Email is required' });
+    }
+    if (!password) {
+      return res.status(400).send({ error: 'Password is required' });
+    }
+
     const user = await UserModel.findOne({ email: login }).select('+password');
     if (!user)
-      return res.status(404).send({ message: 'email/password incorrect' });
-    console.log(user);
+      return res.status(404).send({ error: 'email/password incorrect' });
+
     const isValid = await bcrypt.compare(password, user.password);
     if (!isValid)
-      return res.status(404).send({ message: 'email/password incorrect' });
+      return res.status(404).send({ error: 'email/password incorrect' });
 
     const {
       password: hashedPassword,
@@ -53,7 +66,7 @@ const login = async (req: express.Request, res: express.Response) => {
     } = user.toJSON();
     const token = jwt.sign({ name, email, _id }, process.env.JWT_SECRET);
 
-    res.send({
+    return res.send({
       token,
       user: {
         name,
@@ -63,7 +76,7 @@ const login = async (req: express.Request, res: express.Response) => {
     });
   } catch (error) {
     console.log(error);
-    res.status(500).send();
+    return res.status(500).send();
   }
 };
 export { register, login };
