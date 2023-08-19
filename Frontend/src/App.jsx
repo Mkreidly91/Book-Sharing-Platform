ea695097eda5d46a985017c9770b9ab256ccdb03
Mostafa Kreidly <mkreidly91@gmail.com>
2023-08-19 20:48:51 +0300

    Frontend: Set up routes in app

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,19 @@
 import { useState } from 'react';
 import { Route, Router, Routes } from 'react-router-dom';
 import './App.css';
-
+import SignInForm from './Components/Forms/SignInForm';
+import SignUpForm from './Components/Forms/SignUpForm';
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="app">
       <Routes>
-        <Route path="/" element={<h1>hello</h1>} />
+        <Route path="/" element={<SignInForm className={'bg-red-200'} />} />
+        <Route
+          path="/signUp"
+          element={<SignUpForm className={'bg-red-200'} />}
+        />
       </Routes>
     </div>
   );
