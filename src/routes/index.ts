769efb3447ebaea54de580f2d54d407e8898a3b4
Mostafa Kreidly <mkreidly91@gmail.com>
2023-08-19 12:43:04 +0300

    Backend: added auth route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,9 @@
 import express from 'express';
-import userRoute from './user.route';
+import authRoute from './auth.route';
 
 const router = express.Router();
 
 export default (): express.Router => {
-  userRoute(router);
+  authRoute(router);
   return router;
 };
