04861f1bf14b551dd7cc992d88fc9a1f83387c1f
Mostafa Kreidly <mkreidly91@gmail.com>
2023-08-19 20:50:44 +0300

    Backend: Created and finalized following functionality -  UserController

--- Backend/src/controllers/user.controller.ts
+++ Backend/src/controllers/user.controller.ts
@@ -1,3 +1,42 @@
 import express from 'express';
 import UserModel from '../models/User';
-import bcrypt from 'bcrypt';
+
+import { AuthRequest } from '../middlewares/auth.middleware';
+
+const followUser = async (req: AuthRequest, res: express.Response) => {
+  try {
+    const targetUserId = req.params.userId;
+
+    const currentUserId = req.user._id;
+
+    const targetUser = await UserModel.findById(targetUserId);
+    const currentUser = await UserModel.findById(currentUserId);
+    if (!targetUser) {
+      return res.status(404).send({ message: 'User not found' });
+    }
+
+    const isFollowing = currentUser.following.find((e) =>
+      e._id.equals(targetUser._id)
+    );
+
+    let status;
+    const { following } = currentUser;
+    if (isFollowing) {
+      const filtered = following.filter((e) => !e.equals(targetUser._id));
+      currentUser.following = filtered;
+
+      status = `unfollowed ${targetUser.name}`;
+    } else {
+      currentUser.following = [...following, targetUser._id];
+      status = `followed ${targetUser.name}`;
+    }
+    console.log(currentUser.following);
+    await currentUser.save();
+    return res.status(200).send({ message: status });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({ error });
+  }
+};
+
+export { followUser };
