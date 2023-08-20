4905dee19843998cfdfde0dd3e017ef177c46456
Mostafa Kreidly <mkreidly91@gmail.com>
2023-08-20 13:44:43 +0300

    Frontend: addedd sign in route, modified button csss

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,13 +3,14 @@ import { Route, Router, Routes } from 'react-router-dom';
 import './App.css';
 import SignInForm from './Components/Forms/SignInForm';
 import SignUpForm from './Components/Forms/SignUpForm';
+import SignIn from './Pages/SignIn';
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="app">
+    <div className="app h-full">
       <Routes>
-        <Route path="/" element={<SignInForm className={'bg-red-200'} />} />
+        <Route path="/" element={<SignIn />} />
         <Route
           path="/signUp"
           element={<SignUpForm className={'bg-red-200'} />}
