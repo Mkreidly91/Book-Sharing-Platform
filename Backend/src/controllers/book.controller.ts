adfafcca42d922d81d1a4b36a664f93def4b428c
Mostafa Kreidly <mkreidly91@gmail.com>
2023-08-21 11:57:14 +0300

    Backend: modified search response

--- Backend/src/controllers/book.controller.ts
+++ Backend/src/controllers/book.controller.ts
@@ -27,8 +27,10 @@ const discoverBooks = async (req: AuthRequest, res: express.Response) => {
 const search = async (req: AuthRequest, res: express.Response) => {
   try {
     const { genre, author, keywords } = req.query;
+    const user = req.user;
 
-    // Build the query based on the provided parameters
+    const currentUser = await UserModel.findById(user._id);
+
     const query = {} as any;
 
     if (genre) {
@@ -39,15 +41,32 @@ const search = async (req: AuthRequest, res: express.Response) => {
     }
     if (keywords) {
       query.$or = [
-        { title: { $regex: keywords, $options: 'i' } }, // Case-insensitive search
+        { title: { $regex: keywords, $options: 'i' } },
         { review: { $regex: keywords, $options: 'i' } },
       ];
     }
 
-    // Execute the search query
-    const searchResults = await Book.find(query);
+    const searchResults = await Book.find(query).populate(
+      'createdBy',
+      'name email'
+    );
 
-    return res.status(200).json({ searchResults });
+    let response: any = [];
+    searchResults.forEach((book) => {
+      const isLiked = currentUser.likes.find(
+        (e) => e.toString() === book._id.toString()
+      );
+      const isFollowing = currentUser.following.find(
+        (e) => e.toString() === book.createdBy._id.toString()
+      );
+      response.push({
+        book,
+        isFollowing: isFollowing ? true : false,
+        isLiked: isLiked ? true : false,
+      });
+    });
+
+    return res.status(200).send(response);
   } catch (error) {
     console.error(error);
     return res.status(500).json({ message: 'Internal server error' });
