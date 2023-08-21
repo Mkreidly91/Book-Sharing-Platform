ca901fc6553a7a3fb890e58374937d725d775d84
Mostafa Kreidly <mkreidly91@gmail.com>
2023-08-22 00:56:09 +0300

    Frontend: implemented conditional rendering in card, to account for user's own posts

--- Frontend/src/Components/Posts/Card.jsx
+++ Frontend/src/Components/Posts/Card.jsx
@@ -1,25 +1,30 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useLayoutEffect, useState } from 'react';
 import Button from '../Common/Button';
 import profile from '../../assets/images/profile.png';
 import heartEmpty from '../../assets/icons/heart-empty.svg';
 import heartFull from '../../assets/icons/heart-full.svg';
 import { follow, likeBook } from '../../helpers/user.helpers';
+import { replaceObjectById } from '../../helpers/helpers';
+import { getUser } from '../../helpers/auth.helpers';
 
-const Card = ({ post, cardState }) => {
+const Card = ({ post, setCardState, page, className }) => {
   const { book, isFollowing, isLiked } = post;
   const { _id, title, author, picture, genres, review, createdBy } = book;
-  console.log(post);
   const { _id: userId, name } = createdBy;
+  const { _id: myId } = getUser();
+  const myPost = myId === userId;
+  const [followState, setFollowState] = useState(false);
+  const [likeState, setLikeState] = useState(false);
 
-  const [followState, setFollowState] = useState();
-  const [likeState, setLikeState] = useState();
   useEffect(() => {
     setFollowState(isFollowing);
     setLikeState(isLiked);
-  }, []);
-  console.log(followState);
+  }, [isFollowing, isLiked]);
+
   return (
-    <div className="card-container flex flex-col gap-4 p-5 bg-slate-100 w-[400px] min-w-[400px] min-h-[350px] max-w-[400px] max-h-[350px] rounded-3xl">
+    <div
+      className={`card-container flex flex-col gap-4 p-5 bg-slate-100 w-[400px] min-w-[400px] min-h-[350px] max-w-[400px] max-h-[350px] rounded-3xl ${className}`}
+    >
       <div className="card-header flex justify-between items-center">
         <div className="user flex justify-between items-center gap-2 ">
           <div className="pic-container w-[40px] h-[40px] flex justify-center items-center rounded-full bg-slate-700">
@@ -29,14 +34,35 @@ const Card = ({ post, cardState }) => {
           </div>
           <span className="user-name">{name}</span>
         </div>
-        <Button
-          text={followState ? 'Following' : 'Follow'}
-          className="b-orange text-white py-2 px-4 text-xs rounded-md"
-          onClick={() => {
-            follow(userId);
-            setFollowState((prev) => !prev);
-          }}
-        />
+        {!myPost && (
+          <Button
+            text={followState ? 'Following' : 'Follow'}
+            className="b-orange text-white py-2 px-4 text-xs rounded-md"
+            onClick={() => {
+              follow(userId);
+              setFollowState((prev) => !prev);
+
+              setCardState((prev) => {
+                const newState = prev.map((e) => {
+                  if (e.book.createdBy._id === userId) {
+                    return { ...e, isFollowing: !e.isFollowing };
+                  }
+                  return e;
+                });
+                return newState;
+              });
+
+              if (page.followed) {
+                setCardState((prev) => {
+                  const newState = prev.filter(
+                    (e) => e.book.createdBy._id !== userId
+                  );
+                  return newState;
+                });
+              }
+            }}
+          />
+        )}
       </div>
 
       <div className="card-body grow flex items-start justify-between ">
@@ -57,44 +83,29 @@ const Card = ({ post, cardState }) => {
         </div>
       </div>
       <div className="card-footer flex items-center justify-between">
-        <div className="genre py-2 px-3 bg-blue-300 w-fit rounded-lg">
+        <div className="genre py-2 px-3 bg-blue-300 w-fit rounded-lg text-[11px]">
           {genres.join('/')}
         </div>
-        <img
-          className="cursor-pointer"
-          src={likeState ? heartFull : heartEmpty}
-          alt=""
-          onClick={() => {
-            likeBook(_id);
-            setLikeState((prev) => !prev);
-          }}
-        />
+        {!myPost && (
+          <img
+            className="cursor-pointer"
+            src={likeState ? heartFull : heartEmpty}
+            alt=""
+            onClick={() => {
+              likeBook(_id);
+              setLikeState((prev) => !prev);
+              if (page.liked) {
+                setCardState((prev) => {
+                  const newState = prev.filter((e) => e.book._id !== _id);
+                  return newState;
+                });
+              }
+            }}
+          />
+        )}
       </div>
     </div>
   );
 };
 
 export default Card;
-
-// "book": {
-//   "_id": "64e21f3e364b54f1b0c5f15e",
-//   "title": "Harry Potter and the Sorcerer's Stone",
-//   "author": "J.K. Rowling",
-//   "picture": "https://picsum.photos/200/300",
-//   "genres": [
-//       "Fantasy",
-//       "Adventure",
-//       "Young Adult"
-//   ],
-//   "review": "An enchanting introduction to the magical world of Hogwarts.",
-//   "likes": 0,
-//   "createdBy": {
-//       "_id": "64e128801748f252f650248b",
-//       "name": "John Doe",
-//       "email": "john@gmail.com"
-//   },
-//   "__v": 0
-// },
-// "isFollowing": true,
-// "isLiked": true
-// }
